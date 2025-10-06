7d7c60b2d66931573efdcfbb8b94a6dd0e90eff3
StockSimul User <user@example.com>
2025-10-07 00:37:19 +0900

    Netlify serverless function fix

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
 // Netlify 서버리스 함수 - Express 앱을 Netlify 환경에 맞게 래핑
-const { handler } = require('../../dist/index.js');
+import { handler } from '../../dist/index.js';
 
-exports.handler = handler;
+export { handler };
